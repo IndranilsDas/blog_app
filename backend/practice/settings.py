53bcbe6a66350eab6ff17ac11801b6a54f810c93
Indranil Das <indranildas169@gmail.com>
2025-09-18 20:50:31 +0530

    key

--- backend/practice/settings.py
+++ backend/practice/settings.py
@@ -18,7 +18,7 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost"])
 
